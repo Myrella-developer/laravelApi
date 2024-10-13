740bae17f6ebdb845206261661127d48258d402d
Myrella Almeida <myrella.alvesbcn@outlook.com>
2024-10-13 20:11:05 +0200

    authentication controller y rutas api.php

--- laravelApi/routes/api.php
+++ laravelApi/routes/api.php
@@ -2,7 +2,22 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthenticationController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+//Open Routes
+Route::post("register", [AuthenticationController::class, "register"]);
+Route::post("login", [AuthenticationController::class, "login"]);
+
+//Protected Routes
+Route::group([
+    "middleware" => ["auth:api"]
+], function(){
+
+    Route::get("profile", [AuthenticationController::class, "profile"]);
+    Route::get("logout", [AuthenticationController::class, "logout"]);
+});
+
+// Route::get('/user', function(Request $request) {
+//     return $request->user();
+// })->middleware('auth:api');
+
